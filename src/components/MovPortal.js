8804ef5abbf867b911518df29422d1226e6d2a5c
pthakur11205 <pthakur@uw.edu>
2024-06-18 14:09:09 -0400

    Organizing and properly displaying movie details

--- src/components/MovPortal.js
+++ src/components/MovPortal.js
@@ -43,15 +43,38 @@ function MovPortalComponent() {
           <h4 class="alert-heading">Error!</h4>
           <p class="mb-0">'{enteredText}': '{error}'</p>
         </div>}
+
         {/* Number of results for search query */}
         {movies.length > 0 && <p>Showing {movies.length} results for "{enteredText}"</p>}
-        {JSON.stringify(movies)}
+        
+        {/* For displaying movie details in organized fashion */}
+        {movies.map((movie) => <div key={movie.imdbID}>{getDetails(movie)}</div>)}
       </>
     );
   }
 
-
   
+  // For getting and displaying movie details
+  const getDetails = (movie) => {
+    return (
+        <div className="row">
+          <div className="col-md-2">
+            <img src={movie.Poster} alt={movie.title} width='100%' />
+          </div>
+          <div class='col-md-10'>
+            <div class="card">
+              <div class="card-body">
+                <h3 class="card-title">{movie.Title}</h3>
+                <h6 class="card-subtitle mb-2 text-muted">{movie.Releaed}</h6>
+                <p class="card-text">{movie.Plot}</p>
+                <a href={`https://www.imdb.com/title/${movie.imdbID}`} target="_blank" class="card-link">IMDB</a>
+                <a href={`https://www.youtube.com/results?search_query=${movie.Title} trailer`} target="_blank" class="card-link">Watch Trailer</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  };
 
 
 
